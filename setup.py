02a1bb3ce1621680db1e284ecc0de2673d2eb547
Ian Millington <idmillington@googlemail.com>
2010-09-03 20:20:48 +0100

    Moved subdirectory and added ignore file.

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='django-utils',
+    version="1.0",
+    description="Ian's Django Utility Application",
+    packages=['dj_utils']
+    )
