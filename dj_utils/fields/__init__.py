d5d8240f8b839c6b9aa90cf9ef7a6e9a2ceb66fc
Ian Millington <idmillington@googlemail.com>
2011-01-09 19:13:50 +0000

    Added a restricted slug field.

--- dj_utils/fields/__init__.py
+++ dj_utils/fields/__init__.py
@@ -2,3 +2,4 @@ import ido
 import uuid_field as uuid
 import json_field as json
 import pickle_field as pickle
+import slug
---
+++ dj_utils/fields/slug.py
@@ -0,0 +1,33 @@
+"""
+A more restricted slug field.
+"""
+import django.db.models as models
+import django.forms as forms
+import django.core.validators as validators
+
+default_error = _(
+    u"Slugs must consist of lower case letters, numbers and hyphens, starting "
+    u"with a letter, and ending with a letter or a number."
+    )
+
+# This can be used in place of django.core.validators.slug_re
+restricted_slug_re = re.compile(r'^[-\w]+$')
+
+# This can be used in place of django.core.validators.validate_slug
+validate_resstricted_slug = RegexValidator(
+    restricted_slug_re,
+    default_error,
+    'invalid')
+
+# The slug form field that is used, by default, by the new slug field.
+class RestrictedSlugFormField(forms.SlugField):
+    default_error_messages = {'invalid': default_error}
+    default_validators = [validate_restricted_slug]
+
+# A drop-in replacement for the normal django slug field.
+class RestrictedSlugField(models.SlugField):
+    def formfield(self, **kws):
+        defaults = dict(form_class=RestrictedSlugFormField)
+        defaults.update(kws)
+        return super(RestrictedSlugField, self).formfield(**defaults)
+
