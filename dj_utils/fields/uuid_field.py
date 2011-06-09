6dac686aa6352d301156ded0d18dcf3e4c921bd0
Ian Millington <idmillington@googlemail.com>
2011-06-09 13:40:31 +0100

    Added support for uuid1 and uuid4.

--- dj_utils/fields/uuid_field.py
+++ dj_utils/fields/uuid_field.py
@@ -6,10 +6,16 @@ class UUIDField(models.CharField):
     A field that holds the ISO-standard format of a random 128-bit UUID.
     """
     @staticmethod
-    def rnd_uuid():
+    def rnd_uuid1():
+        """Generate a random UUID1"""
+        return str(uuid.uuid1())
+
+    @staticmethod
+    def rnd_uuid4():
+        """Generate a random UUID4 (the default for the field)."""
         return str(uuid.uuid4())
 
     def __init__(self, *args, **kws):
-        kws.setdefault('default', UUIDField.rnd_uuid)
+        kws.setdefault('default', UUIDField.rnd_uuid4)
         kws.setdefault('max_length', 36)
         super(UUIDField, self).__init__(*args, **kws)
