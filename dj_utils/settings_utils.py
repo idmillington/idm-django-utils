199881294a9c058e8e3a0c33ac4cc58c49e37957
Ian Millington <idmillington@googlemail.com>
2011-08-23 02:45:57 +0100

    Added support for global machine-id

--- dj_utils/settings_utils.py
+++ dj_utils/settings_utils.py
@@ -8,31 +8,29 @@ def environment():
         if 'DJANGO_MACHINE_ID' in os.environ:
             _id = os.environ['DJANGO_MACHINE_ID']
         else:
-            try:
-                file = '.machine_id'
-                # try current working directory
-                if os.path.isfile(file):
-                    _id = open(file).read().strip()
-                else:
-                    # hunt in the sys.path
-                    for d in sys.path:
-                        filename = os.path.join(d, file)
-                        if os.path.isfile(filename):
-                            _id = open(filename).read().strip()
-                            break
-                    else:
-                        raise IOError
-            except IOError:
+            # Look in the current directory, then the user's home,
+            # then the python path.
+            paths = [".", os.path.expanduser("~")] + sys.path
+            for path in paths:
+                fn = os.path.join(path, '.machine_id')
+                if os.path.isfile(fn):
+                    _id = open(fn).read().strip()
+                    break
+            else:
                 _id = 'development'
                 import textwrap
                 print '\n'.join(textwrap.wrap(textwrap.dedent("""
-                    Running as development server. If this is a
-                    production machine, set the DJANGO_MACHINE_ID
-                    environment variable (e.g. in the WSGI, shell
-                    or mod_python configuration).  Alternatively
-                    create a file called .machine_id containing an
-                    id for your machine in the CWD or sys.path.
-                    """), 76))
+                    Running as development server with machine id
+                    '%s'. You should set this explicitly. The best way
+                    is to set the DJANGO_MACHINE_ID environment
+                    variable (e.g. in the WSGI, shell or mod_python
+                    configuration). Alternatively create a file called
+                    '.machine_id' containing just the id. The
+                    '.machine_id' file can go in the CWD, in your home
+                    directory, or in sys.path: the environment
+                    variable setting takes priority, then the paths
+                    are searched in that order.
+                    """ % _id), 76))
     return _id
 
 def config(default=None, environment=environment(), **values):
