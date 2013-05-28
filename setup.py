f045d20f4420f64432bd012c6164e8751d408d30
idmillington <idmillington@gmail.com>
2013-05-28 19:47:23 +0100

    Filled out package info.

--- setup.py
+++ setup.py
@@ -4,7 +4,12 @@ from distutils.core import setup
 
 setup(
     name='django-utils',
+
     version="0.2.2",
+    author='Ian Millington',
+    author_email='idmillington@googlemail.com',
+
+    url="https://github.com/idmillington/django_utils",
     description="Ian's Django Utility Application",
     packages=['dj_utils', 'dj_utils.fields']
     )
